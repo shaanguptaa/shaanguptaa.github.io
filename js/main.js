06ee0844c6acba80c5c7611c10343c76b92d2696
Sahil Gupta <guptasahil670@gmil.com>
2022-07-14 00:41:14 +0530

    Added Navbar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+// nav-menu
+$('#menu').on('click', function () {
+    $('.bars', this).toggleClass('cross-icon');
+    $('.nav-content').toggleClass('active');
+});
+
+
+// particles and one-scroll
 $(document).ready(function() {
     particlesJS.load('particles-js', 'js/particles.json', function() {
         console.log('callback - particles.js config loaded');
